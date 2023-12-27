1bab7343315389b0ffc634041fc83bf7583c1e98
ADITYA-KR-04 <ad61204@gmail.com>
2023-12-27 18:17:04 +0530

    First Array

---
+++ firstArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int daysinmonths[4] = {28, 29, 30, 31};
+    cout << "Number of days a month can have" << endl;
+    // For loop
+    // for (int i=0;i< 4; i++){
+    //     cout<<i+1<<"."<<monthsindays[i]<<endl;
+    // }
+    // While loop;
+    // int i = 0;
+    //    while (i< 4)
+    //    {
+    //     cout<<i+1<<"."<<daysinmonths[i]<<endl;
+    //     i++;
+    //       }
+    int i = 0;
+    do
+    {
+        cout << i + 1 << "." << daysinmonths[i] <<endl;
+        i++;
+    } while (i < 4);
+
+    return 0;
+}
